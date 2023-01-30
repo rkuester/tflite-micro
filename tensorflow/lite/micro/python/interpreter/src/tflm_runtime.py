6e0cd21f95df69de3d00d657f6b141a88f5ad2d8
Ryan Kuester <kuester@bdti.com>
2023-01-30 16:15:33 -0600

    refactor: allow inheritance from py interpreter
    Fix the py interpreter factory functions so they return a derived
    class when appropriate by using the passed class object rather
    than hardcoding the class name. This allows inheritance.

--- tensorflow/lite/micro/python/interpreter/src/tflm_runtime.py
+++ tensorflow/lite/micro/python/interpreter/src/tflm_runtime.py
@@ -44,7 +44,7 @@ class Interpreter(object):
         model_data, custom_op_registerers, arena_size, num_resource_variables)
 
   @classmethod
-  def from_file(self,
+  def from_file(cls,
                 model_path,
                 custom_op_registerers=[],
                 arena_size=None,
@@ -70,10 +70,10 @@ class Interpreter(object):
     with open(model_path, "rb") as f:
       model_data = f.read()
 
-    return Interpreter(model_data, custom_op_registerers, arena_size)
+    return cls(model_data, custom_op_registerers, arena_size)
 
   @classmethod
-  def from_bytes(self,
+  def from_bytes(cls,
                  model_data,
                  custom_op_registerers=[],
                  arena_size=None,
@@ -94,7 +94,7 @@ class Interpreter(object):
       An Interpreter instance
     """
 
-    return Interpreter(model_data, custom_op_registerers, arena_size)
+    return cls(model_data, custom_op_registerers, arena_size)
 
   def print_allocations(self):
     """Invoke the RecordingMicroAllocator to print the arena usage.
